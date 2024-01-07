4b79e333bf9eee0bdb1db6d44423e32e33bda50e
abderrazak-Bahaj <bahajabde0@gmail.com>
2024-01-07 12:05:59 -0800

    edt ExperienceAndEducation

--- src/sections/ExperienceAndEducation/index.tsx
+++ src/sections/ExperienceAndEducation/index.tsx
@@ -2,43 +2,38 @@ import React from "react";
 import { Timeline} from "@/components";
 import { IconExperience, IconEduction } from '@/assets/icons';
 
-
-const ExperienceAndEducation: React.FC = () => {
-  const fakeDataExperience = [
+ const experience = [
     {
-      title: "Developer web and mobile",
-      date: "2018-2021",
-      description: "All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.",
-    },
-    {
-      title: "fullstack developer",
-      date: "2021-2022",
-      description: "All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.",
+      title: "Fullstack developer",
+      date: "07-2022-Present",
+      description: "developing fullstack applications for clients and organizations. We are working on different projects with different technologies. ",
     },
   ]
-  
-  const fakeDataEductions = [
+  const eductions = [
     {
-      title: "Bachelor of Science",
-      date: "2010-2013",
-      description: "All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.",
+      title: "Bachelor of Applied Science",
+      date: "2016 - 2017",
+      description: "Lycee Moula Abdellah Ben HssainLycee Moula Abdellah Ben Hssain",
     },
     {
-      title: "License to development software",
-      date: "2013-2015",
-      description: "All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.",
+      title: "University, Chemistry",
+      date: "2017 - 2018",
+      description: "Faculty of Sciences of AgadirFaculty of Sciences of Agadire",
     },
     {
-      title: "Master of Science",
-      date: "2015-2018",
-      description: "All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.",
+      title: "Specialized Technician, Computer Development",
+      date: "2018 - 2020",
+      description: "Specialized Institute Of Applied Technology Tinghir Moroccan",
     }
   ]
+
+const ExperienceAndEducation: React.FC = () => {
+ 
   return (
     <section className="py-8 ">
       <div className="w-full px-4  my-5 pt-5">
             <div className="mx-auto mb-12 max-w-[510px] text-center lg:mb-20">
-              <h2 className="mb-3 text-3xl font-bold leading-[1.2] text-dark dark:text-white sm:text-4xl md:text-[40px]">
+              <h2 className="mb-3 text-3xl font-bold leading-[1.2] text-dark dark:text-white sm:text-4xl md:text-4xl">
                 Experience & Education
               </h2>
             </div>
@@ -47,11 +42,11 @@ const ExperienceAndEducation: React.FC = () => {
       <div className="flex flex-wrap mx-5 my-5" >
         <div className="w-full px-4 md:w-1/2">
           <h3 className="mb-[30px] text-3xl font-bold leading-[1.2] text-dark dark:text-white sm:text-2xl md:text-[30px]">Experience</h3>
-          <Timeline  items={fakeDataExperience}   icon={ <IconExperience />} />
+          <Timeline  items={experience}   icon={ <IconExperience />} />
         </div>
         <div className="w-full px-4 md:w-1/2">
           <h3 className="mb-[30px] text-3xl font-bold leading-[1.2] text-dark dark:text-white sm:text-2xl md:text-[30px]">Education</h3>
-          <Timeline items={fakeDataEductions} icon={<IconEduction />} />
+          <Timeline items={eductions} icon={<IconEduction />} />
         </div>
       </div>
     </section>
